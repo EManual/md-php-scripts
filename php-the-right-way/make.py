d3585f5b2980d7e0046de7cd8eb43660dab0e518
Jayin Ton <tonjayin@gmail.com>
2015-03-13 14:28:09 +0800

    done

--- php-the-right-way/make.py
+++ php-the-right-way/make.py
@@ -9,7 +9,7 @@
 
 # 读取文件内容
 # - 去除头部信息
-#   - 获取文件标题，第一个h1标签`# title` or `## title`
+# - 获取文件标题，第一个h1标签`# title` or `## title`
 
 
 from path import Path
@@ -17,9 +17,15 @@ import os
 import sys
 import re
 import codecs
+import shutil
 
 source_path = u'./php-the-right-way/_posts/'  # markdown文件源
 dest_path = u'./dist'  # 生成到
+
+if os.path.exists(dest_path):
+    shutil.rmtree(dest_path)
+
+os.mkdir(dest_path)
 
 p = Path(source_path)
 
@@ -27,6 +33,11 @@ regex_title = re.compile(r'^[#]+(.*){#')
 
 
 def __get_real_content_lines(file_path):
+    """
+    获得文件(jekyll)主体内容
+    :param file_path:
+    :return:
+    """
     with codecs.open(file_path, encoding='utf-8') as f:
         lines = f.readlines()
         if '---' in lines[0]:
@@ -39,7 +50,6 @@ def __get_real_content_lines(file_path):
             regex_white_space = re.compile('^\s+$')
             while '' == regex_white_space.sub('', lines[i]):
                 i += 1
-            # print ''.join(lines[i:])
             return lines[i:]
 
     return lines
@@ -49,7 +59,6 @@ def parser_file_name(file_path):
     """
     解析文件名
     :param file_path:
-    :return:
     """
     # print os.path.dirname(file_path)
     filename = os.path.basename(file_path)
@@ -62,12 +71,11 @@ def parser_file_name(file_path):
     # print regex_rm_anchor.findall(content_lines[0])[0]
     content_lines[0] = regex_rm_anchor.findall(content_lines[0])[0]
 
-
     return {
         'a': spliters[0],
         'b': spliters[1],
         'c': spliters[2],
-        'content': ''.join(content_lines)
+        'content': u''.join(content_lines)
     }
 
 
@@ -77,10 +85,58 @@ def get_file_title(file_path):
     return regex_title.findall(lines[0])[0].strip(' ')
 
 
-for f in p.listdir():
-    file_info = parser_file_name(f)
-    title = get_file_title(f)
+def filter_content(content):
+    """
+    过滤/修正部分字符串
+    :param content:
+    :return:
+    """
+    regex_highlight = re.compile(r'(\{% highlight ([a-zA-Z]+) %\}){1}')
 
-    print title
-    print file_info['content']
-    # break
+    for x in regex_highlight.findall(content):
+        content = content.replace(x[0], '```%s' % x[1])
+
+    return content.replace('{% endhighlight %}', '```')
+
+
+def write_file(dir_name, name, content):
+    """
+    写入文件
+    :param dir_name: 目录名
+    :param name: 文件名
+    :param content: 内容
+    """
+    with codecs.open(os.path.join(dest_path, dir_name, name + u'.md'), mode='w', encoding='utf-8') as f:
+        f.write(content)
+
+
+def main():
+    cur_dir = ''
+    cur_a = '0'
+    cur_b = '0'
+    cur_c = '0'
+
+    dir_index = 1
+    file_index = 0
+
+    for f in p.listdir():
+        file_info = parser_file_name(f)
+        file_info['content'] = filter_content(file_info['content'])
+
+        title = get_file_title(f)
+
+        file_index += 1
+        if file_info['b'] == '01':
+            cur_dir = '%04d-%s' % (dir_index, title)
+            os.mkdir(os.path.join(dest_path, cur_dir))
+            dir_index += 1
+            file_index = 1
+
+        write_file(cur_dir, u'%04d-%s' % (file_index, title), file_info['content'])
+
+    print('Finish!')
+
+
+if __name__ == '__main__':
+    main()
+
